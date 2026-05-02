9c24dde7d7966728b97976429cbe69bf381adc71
LauEroles <laura.eroles@gmail>
2026-05-02 20:36:52 -0300

    generate job-offers resource with module, controller, service, DTOs and entity

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { JobOffersModule } from './job-offers/job-offers.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, JobOffersModule],
   controllers: [AppController],
   providers: [AppService],
 })
