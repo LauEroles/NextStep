a6d29fffe71279f397e4d1a34b1b13a49bf48dbc
LauEroles <laura.eroles@gmail>
2026-05-02 22:47:29 -0300

    add TypeORM configuration to app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { JobOffersModule } from './job-offers/job-offers.module';
 
 @Module({
-  imports: [UsersModule, JobOffersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'lauraeroles',
+      password: '',
+      database: 'nextstepdb',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule,
+    JobOffersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
