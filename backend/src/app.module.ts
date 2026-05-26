2de2ea1e2a198b9f1a0400dc2ed0cb515143f0ff
Melissa <lissak230@gmail.com>
2026-05-25 22:48:52 -0300

    database config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,32 +1,35 @@
 import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-// import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+import { UsersModule } from './users/users.module';
 // import { JobOffersModule } from './job-offers/job-offers.module';
 // import { JobApplicationsModule } from './job-applications/job-applications.module';
 
 @Module({
   imports: [
-    // ConfigModule.forRoot({
-    //   isGlobal: true,
-    // }),
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: process.env.DB_HOST,
-    //   port: parseInt(process.env.DB_PORT || '5432', 10),
-    //   username: process.env.DB_USER,
-    //   password: process.env.DB_PASSWORD,
-    //   database: process.env.DB_NAME,
-    //   autoLoadEntities: true,
-    //   synchronize: true,
-    // }),
-    // UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+
+      ssl: true,
+      extra: {
+        ssl: { rejectUnauthorized: false },
+      },
+
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    UsersModule,
     // JobOffersModule,
     // JobApplicationsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
