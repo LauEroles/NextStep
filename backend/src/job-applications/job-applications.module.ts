7563fe1cdbcd4fe92ed66e99b994a048a0ebff2b
Melissa <lissak230@gmail.com>
2026-05-26 02:38:48 -0300

    job applications module

--- backend/src/job-applications/job-applications.module.ts
+++ backend/src/job-applications/job-applications.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { JobApplicationsService } from './job-applications.service';
 import { JobApplicationsController } from './job-applications.controller';
+import { JobApplication } from './entities/job-application.entity';
+import { UsersModule } from '../users/users.module';
+import { JobOffersModule } from '../job-offers/job-offers.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([JobApplication]),
+    UsersModule,
+    JobOffersModule,
+  ],
   controllers: [JobApplicationsController],
   providers: [JobApplicationsService],
+  exports: [JobApplicationsService],
 })
 export class JobApplicationsModule {}
