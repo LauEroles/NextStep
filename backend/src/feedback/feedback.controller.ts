1c97ad96b8dae2ef23aaa1434e4fc8be733cbb4c
LauEroles <laura.eroles@gmail>
2026-05-26 00:50:02 -0300

    add feedback controller

--- backend/src/feedback/feedback.controller.ts
+++ backend/src/feedback/feedback.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { FeedbackService } from './feedback.service';
 import { CreateFeedbackDto } from './dto/create-feedback.dto';
 import { UpdateFeedbackDto } from './dto/update-feedback.dto';
