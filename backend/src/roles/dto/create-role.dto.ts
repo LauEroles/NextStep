ed92a074d1bbc36fe6e22f8b2447b11e33baa3e3
melissa-kessler <lissak230@gmail.com>
2026-05-31 02:50:42 -0300

    create role dto

--- backend/src/roles/dto/create-role.dto.ts
+++ backend/src/roles/dto/create-role.dto.ts
@@ -1,1 +1,7 @@
-export class CreateRoleDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateRoleDto {
+  @IsString()
+  @IsNotEmpty({ message: 'El nombre del rol es obligatorio' })
+  name: string;
+}
