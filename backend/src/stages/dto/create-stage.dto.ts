3491208e7ec18bf14d179efec20ac715af5c3180
melissa-kessler <lissak230@gmail.com>
2026-06-01 19:56:33 -0300

    create stage dto

--- backend/src/stages/dto/create-stage.dto.ts
+++ backend/src/stages/dto/create-stage.dto.ts
@@ -1,1 +1,12 @@
-export class CreateStageDto {}
+import { IsInt, IsNotEmpty, IsString, Min } from 'class-validator';
+
+export class CreateStageDto {
+  @IsNotEmpty({ message: 'El nombre de la etapa es obligatorio.' })
+  @IsString({ message: 'El nombre de la etapa debe ser un texto válido.' })
+  name: string;
+
+  @IsNotEmpty({ message: 'El orden de secuencia es obligatorio.' })
+  @IsInt({ message: 'El orden de secuencia debe ser un número entero.' })
+  @Min(1, { message: 'El orden de secuencia mínimo debe ser 1.' })
+  sequence_order: number;
+}
