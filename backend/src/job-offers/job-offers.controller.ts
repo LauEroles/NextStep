0cf01c41e939fcf2140d23de2529af03d59d0981
melissa-kessler <lissak230@gmail.com>
2026-06-01 20:06:52 -0300

    add roles guard to job offers

--- backend/src/job-offers/job-offers.controller.ts
+++ backend/src/job-offers/job-offers.controller.ts
@@ -12,27 +12,33 @@ import { JobOffersService } from './job-offers.service';
 import { CreateJobOfferDto } from './dto/create-job-offer.dto';
 import { UpdateJobOfferDto } from './dto/update-job-offer.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('job-offers')
 export class JobOffersController {
   constructor(private readonly jobOffersService: JobOffersService) {}
 
+  @Roles('recruiter')
   @Post()
   create(@Body() createJobOfferDto: CreateJobOfferDto) {
     return this.jobOffersService.create(createJobOfferDto);
   }
 
+  @Roles('admin', 'recruiter', 'applicant')
   @Get()
   findAll() {
     return this.jobOffersService.findAll();
   }
 
+  @Roles('admin', 'recruiter', 'applicant')
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.jobOffersService.findOne(+id);
   }
 
+  @Roles('recruiter')
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -41,6 +47,7 @@ export class JobOffersController {
     return this.jobOffersService.update(+id, updateJobOfferDto);
   }
 
+  @Roles('admin', 'recruiter')
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.jobOffersService.remove(+id);
