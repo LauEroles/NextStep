6368bfad389a6f7eb565870ae3fb12a2a3745866
melissa-kessler <lissak230@gmail.com>
2026-06-01 19:46:24 -0300

    create stage entity

--- backend/src/stages/entities/stage.entity.ts
+++ backend/src/stages/entities/stage.entity.ts
@@ -1,1 +1,13 @@
-export class Stage {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('stages')
+export class Stage {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  name: string;
+
+  @Column({ name: 'sequence_order', type: 'int', unique: true })
+  sequenceOrder: number;
+}
