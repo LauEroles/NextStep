3fd1cb1192ce7ddc68f41733191fe7226eaa8196
Melissa <lissak230@gmail.com>
2026-06-04 04:30:40 -0300

    update job offer dto and seniority enum

--- backend/src/job-offers/dto/update-job-offer.dto.ts
+++ backend/src/job-offers/dto/update-job-offer.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateJobOfferDto } from './create-job-offer.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateJobOfferDto extends PartialType(CreateJobOfferDto) {}
+export class UpdateJobOfferDto extends PartialType(CreateJobOfferDto) {
+  @IsOptional()
+  @IsBoolean({ message: 'El campo isActive debe ser verdadero o falso' })
+  isActive?: boolean;
+}
