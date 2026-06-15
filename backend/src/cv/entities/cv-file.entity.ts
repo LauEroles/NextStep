c10330ad45d7a64c5d348acd4c5869a5e5a776aa
LauEroles <laura.eroles@gmail>
2026-06-15 02:15:16 -0300

    install multer and cv-file entity created

---
+++ backend/src/cv/entities/cv-file.entity.ts
@@ -0,0 +1,34 @@
+// src/cv/entities/cv-file.entity.ts
+import {
+  Entity, PrimaryGeneratedColumn, Column,
+  ManyToOne, CreateDateColumn, JoinColumn
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+
+@Entity('cv_files')
+export class CvFile {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  originalName: string;   // nombre original que mandó el usuario
+
+  @Column()
+  storedName: string;     // nombre con UUID en disco: uuid-originalName.ext
+
+  @Column()
+  extension: string;      // 'pdf', 'docx', etc.
+
+  @Column()
+  directory: string;      // ruta relativa: 'uploads/cv'
+
+  @ManyToOne(() => User,{ onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @Column({name: 'user_id', type: 'int'})
+  userId: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
