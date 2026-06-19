b49054466298bfb16f46407f1fe8a3fc4ea66598
Melissa <lissak230@gmail.com>
2026-06-19 01:17:38 -0300

    install swagger ui and add in main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,25 +4,44 @@ import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.use(helmet());
+
   app.enableCors({
     origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
-  app.useStaticAssets(join(process.cwd(), 'uploads'), { // ← agregá esto
+
+  app.useStaticAssets(join(process.cwd(), 'uploads'), {
     prefix: '/uploads',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('NextStep API')
+    .setDescription('Documentación oficial del backend de NextStep')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  if (process.env.NODE_ENV !== 'production') {
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api-docs', app, document);
+  }
+
   await app.listen(process.env.PORT ?? 3001);
 }
+
 bootstrap();
